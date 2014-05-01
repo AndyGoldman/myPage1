a03669a6399eaeb02d106b613bba87d8d816dfcb
AndyGoldman <andy.goldman@hotmail.com>
2014-04-30 21:42:40 -0600

    Fixed jQuery call
    li was not child of input.  Added class to ul that could be called by
    jQuery

--- myPage1a.js
+++ myPage1a.js
@@ -1,4 +1,11 @@
-$(document).ready(function () {
-	$('.menuButton').mouseenter(function () {
-		$(this).children('.jQueryButton').slideDown('fast');
-	}
+$(document).ready(function (){
+	$('.navButton').mouseenter(function (){
+		$(this).children('.submenu').slideToggle('slow');
+	});
+		$('.navButton').mouseleave(function (){
+		$(this).children('.submenu').slideToggle('slow');
+	});
+	$('.submenuButton').mouseenter(function (){
+		$(this).children('.submenu').show();
+	});
+});
